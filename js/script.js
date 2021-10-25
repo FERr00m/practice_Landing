ca32d1e66bf7ebea222ac9bc45a9acaec1adb938
Алексей Н. Мельников <u183@stekspb.ru>
2021-10-25 16:07:32 +0300

    add swiper.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,38 @@
 window.addEventListener('DOMContentLoaded', function() {
 	try {
-		//...
+		const swiper = new Swiper('.swiper', {
+			// Optional parameters
+			direction: 'horizontal',
+			loop: true,
+			freeMode: true,
+			autoplay: {
+				delay: 2500,
+				disableOnInteraction: false,
+			},
+			slidesPerView: 6,
+			spaceBetween: 74,
+			pagination: {
+				el: ".swiper-pagination",
+				clickable: true,
+			},
+			breakpoints: {
+				// when window width is >= 320px
+				320: {
+					slidesPerView: 2,
+					spaceBetween: 20
+				},
+				// when window width is >= 480px
+				480: {
+					slidesPerView: 3,
+					spaceBetween: 30
+				},
+				// when window width is >= 640px
+				640: {
+					slidesPerView: 6,
+					spaceBetween: 74
+				}
+			}
+		});
 	} catch (error) {
 		console.error(error);
 	}
